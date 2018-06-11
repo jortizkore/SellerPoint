fc350a3fc1453df9b3e37f1e4cd1557cd0a01670
JoynerOrtiz9313 <jwot.9313@outlook.es>
2018-06-10 20:15:22 -0400

    fromhome1 vista prod added

--- J_SellPoint/Forms/Maintenances/FrmProducts.Designer.cs
+++ J_SellPoint/Forms/Maintenances/FrmProducts.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TxtFilterProducts = new System.Windows.Forms.TextBox();
             this.GrdProduct = new System.Windows.Forms.DataGridView();
             this.RefreshProd = new System.Windows.Forms.Button();
             this.tabPage1 = new System.Windows.Forms.TabPage();
@@ -41,11 +43,24 @@
             this.TxtPrecio = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.TxtFilterProducts = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.LblQuantity = new System.Windows.Forms.Label();
+            this.TxtPriceDetails = new System.Windows.Forms.TextBox();
+            this.LblCosto = new System.Windows.Forms.Label();
+            this.LblDesc = new System.Windows.Forms.Label();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CdmAddOne = new System.Windows.Forms.Button();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.GrdProduct)).BeginInit();
             this.tabPage1.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabPage2
@@ -56,10 +71,18 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(463, 236);
+            this.tabPage2.Size = new System.Drawing.Size(542, 236);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Products in Stock";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TxtFilterProducts
+            // 
+            this.TxtFilterProducts.Location = new System.Drawing.Point(100, 7);
+            this.TxtFilterProducts.Name = "TxtFilterProducts";
+            this.TxtFilterProducts.Size = new System.Drawing.Size(166, 20);
+            this.TxtFilterProducts.TabIndex = 3;
+            this.TxtFilterProducts.TextChanged += new System.EventHandler(this.TxtFilterProducts_TextChanged);
             // 
             // GrdProduct
             // 
@@ -71,8 +94,9 @@
             this.GrdProduct.Location = new System.Drawing.Point(6, 35);
             this.GrdProduct.Name = "GrdProduct";
             this.GrdProduct.ReadOnly = true;
-            this.GrdProduct.Size = new System.Drawing.Size(451, 195);
+            this.GrdProduct.Size = new System.Drawing.Size(530, 195);
             this.GrdProduct.TabIndex = 2;
+            this.GrdProduct.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.GrdProduct_CellMouseDoubleClick);
             // 
             // RefreshProd
             // 
@@ -97,7 +121,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(463, 236);
+            this.tabPage1.Size = new System.Drawing.Size(542, 236);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Add product";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -105,7 +129,7 @@
             // TxtCancel
             // 
             this.TxtCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.TxtCancel.Location = new System.Drawing.Point(378, 192);
+            this.TxtCancel.Location = new System.Drawing.Point(457, 192);
             this.TxtCancel.Name = "TxtCancel";
             this.TxtCancel.Size = new System.Drawing.Size(79, 38);
             this.TxtCancel.TabIndex = 9;
@@ -179,25 +203,144 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(471, 262);
+            this.tabControl1.Size = new System.Drawing.Size(550, 262);
             this.tabControl1.TabIndex = 8;
             // 
-            // TxtFilterProducts
+            // tabPage3
             // 
-            this.TxtFilterProducts.Location = new System.Drawing.Point(100, 7);
-            this.TxtFilterProducts.Name = "TxtFilterProducts";
-            this.TxtFilterProducts.Size = new System.Drawing.Size(166, 20);
-            this.TxtFilterProducts.TabIndex = 3;
-            this.TxtFilterProducts.TextChanged += new System.EventHandler(this.TxtFilterProducts_TextChanged);
+            this.tabPage3.Controls.Add(this.CdmAddOne);
+            this.tabPage3.Controls.Add(this.LblQuantity);
+            this.tabPage3.Controls.Add(this.TxtPriceDetails);
+            this.tabPage3.Controls.Add(this.LblCosto);
+            this.tabPage3.Controls.Add(this.LblDesc);
+            this.tabPage3.Controls.Add(this.TxtID);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(542, 236);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Details";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // LblQuantity
+            // 
+            this.LblQuantity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblQuantity.AutoSize = true;
+            this.LblQuantity.Location = new System.Drawing.Point(477, 24);
+            this.LblQuantity.Name = "LblQuantity";
+            this.LblQuantity.Size = new System.Drawing.Size(13, 13);
+            this.LblQuantity.TabIndex = 9;
+            this.LblQuantity.Text = "0";
+            // 
+            // TxtPriceDetails
+            // 
+            this.TxtPriceDetails.Location = new System.Drawing.Point(54, 183);
+            this.TxtPriceDetails.Name = "TxtPriceDetails";
+            this.TxtPriceDetails.Size = new System.Drawing.Size(112, 20);
+            this.TxtPriceDetails.TabIndex = 8;
+            this.TxtPriceDetails.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPrice_KeyPress);
+            // 
+            // LblCosto
+            // 
+            this.LblCosto.AutoSize = true;
+            this.LblCosto.Location = new System.Drawing.Point(20, 153);
+            this.LblCosto.Name = "LblCosto";
+            this.LblCosto.Size = new System.Drawing.Size(13, 13);
+            this.LblCosto.TabIndex = 7;
+            this.LblCosto.Text = "--";
+            // 
+            // LblDesc
+            // 
+            this.LblDesc.AutoSize = true;
+            this.LblDesc.Location = new System.Drawing.Point(20, 89);
+            this.LblDesc.Name = "LblDesc";
+            this.LblDesc.Size = new System.Drawing.Size(13, 13);
+            this.LblDesc.TabIndex = 6;
+            this.LblDesc.Text = "--";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(44, 17);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(75, 20);
+            this.TxtID.TabIndex = 5;
+            this.TxtID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtID_KeyPress);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(427, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Quantity";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Cost";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "ID";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // CdmAddOne
+            // 
+            this.CdmAddOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CdmAddOne.Location = new System.Drawing.Point(430, 40);
+            this.CdmAddOne.Name = "CdmAddOne";
+            this.CdmAddOne.Size = new System.Drawing.Size(43, 23);
+            this.CdmAddOne.TabIndex = 10;
+            this.CdmAddOne.Text = "+1";
+            this.CdmAddOne.UseVisualStyleBackColor = true;
+            this.CdmAddOne.Click += new System.EventHandler(this.CdmAddOne_Click);
             // 
             // FrmProducts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 284);
+            this.ClientSize = new System.Drawing.Size(568, 284);
             this.Controls.Add(this.tabControl1);
             this.Name = "FrmProducts";
             this.Text = "Product Maintenance";
@@ -208,6 +351,8 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -228,5 +373,18 @@
         private System.Windows.Forms.Button RefreshProd;
         private System.Windows.Forms.DataGridView GrdProduct;
         private System.Windows.Forms.TextBox TxtFilterProducts;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label LblQuantity;
+        private System.Windows.Forms.TextBox TxtPriceDetails;
+        private System.Windows.Forms.Label LblCosto;
+        private System.Windows.Forms.Label LblDesc;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button CdmAddOne;
     }
 }
--- J_SellPoint/Forms/Maintenances/FrmProducts.cs
+++ J_SellPoint/Forms/Maintenances/FrmProducts.cs
@@ -14,7 +14,7 @@ namespace J_SellPoint.Forms.Maintenances
 {
     public partial class FrmProducts : Form
     {
-        private IProduct prod;
+        private Product prod;
         public FrmProducts()
         {
             InitializeComponent();
@@ -72,6 +72,74 @@ namespace J_SellPoint.Forms.Maintenances
         private void TxtFilterProducts_TextChanged(object sender, EventArgs e)
         {
             LoadRefreshProdGrid(TxtFilterProducts.Text);
+        }
+
+        private void TxtID_KeyPress(object sender, KeyPressEventArgs e)
+
+        {
+            if(e.KeyChar == 13)
+            {
+                LoadProduct(int.Parse(TxtID.Text));
+            }
+        }
+
+        private void LoadProduct(int id)
+        {
+            prod.Load(id);
+            LblDesc.Text = prod.Description;
+            LblCosto.Text = prod.Cost.ToString();
+            TxtPriceDetails.Text = prod.Price.ToString();
+            LblQuantity.Text = prod.Quantity.ToString();
+        }
+
+        private void CdmAddOne_Click(object sender, EventArgs e)
+        {
+            prod = new Product();
+            prod.Load(int.Parse(TxtID.Text));
+            if(prod != null)
+            {
+                prod.AddQuantity(prod.Id, 1);
+            }
+            RefreshAll();
+        }
+
+        void RefreshAll()
+        {
+            LoadProduct(prod.Id);
+            LoadRefreshProdGrid();
+        }
+
+        private void TxtPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (UpdateProduct())
+                {
+                    Program.DisplayInfoMessage("Actualizado! :D");
+                }
+                else
+                {
+                    Program.DisplayErrorMessage("Error al actualizar :(");
+                }
+                RefreshAll();
+            }
+        }
+
+        private bool UpdateProduct()
+        {
+            prod = new Product();
+            prod.Load(int.Parse(TxtID.Text));
+            prod.Price = decimal.Parse(TxtPriceDetails.Text);
+            return prod.Update();
+        }
+
+        private void GrdProduct_CellMouseDoubleClick(object sender,
+            DataGridViewCellMouseEventArgs e)
+        {
+            var productid = int.Parse(GrdProduct.Rows[e.RowIndex].Cells[0].Value.ToString());
+            TxtID.Text = productid.ToString();
+            LoadProduct(productid);
+            tabControl1.SelectTab(2);
         }
     }
 }
